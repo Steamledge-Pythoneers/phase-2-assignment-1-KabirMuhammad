43c1c2cc800edbba6ff9e1ba0c54e3f8a1c2b3a7
kabir sani <kabirsani39@gmail.com>
2020-09-24 19:30:19 +0100

    Calculate lowest term of numbers

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,4 +1,19 @@
 ## TODO: complete the function "lowest_terms" below
+from math import gcd 
+
 
 def lowest_terms(x):
-	return ""
+	num, den = x.split('/')
+	num = int(num)
+	den = int(den)
+	x = reduceFraction(num, den)
+	return x 
+
+	
+
+def reduceFraction(num, den):
+    d = gcd(num, den)
+    num = num // d
+    den = den // d
+    x = str(num) + '/' + str(den)
+    return x
